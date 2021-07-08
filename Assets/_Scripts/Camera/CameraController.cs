76608c8afe411b57af50073ea9617a200b0b4cde
Vinícius Veronese Gonçalves <vi.veronese@gmail.com>
2021-07-08 14:52:01 -0300

    Added camera zoom

--- Assets/_Scripts/Camera/CameraController.cs
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Settings")]
+    public float scroolSpeed = 5f;
+    public float zoomYMin = 7f;
+    public float zoomYMax = 14f;
+    [Header("References")]
     public Transform player;
+
+    private Transform cam;
     private PlayerController playerController;
 
     private void Start(){
+        cam = transform.GetChild(0);
         playerController = player.GetComponent<PlayerController>();
+    }
+
+    private void Update(){
+        Zoom();
+        MoveOnClick();
     }
 
     private void LateUpdate(){
         transform.position = player.position;
     }
 
-    private void Update(){
-        MoveOnClick();
+    private void Zoom () {
+        float scrool = Input.GetAxis("Mouse ScrollWheel");
+
+        if (cam.localPosition.y <= zoomYMin) {
+            scrool = Mathf.Min(0f, scrool);
+        } else if (cam.localPosition.y >= zoomYMax) {
+            scrool = Mathf.Max(0f, scrool);
+        }
+
+        cam.localPosition += cam.forward * scrool * scroolSpeed;
     }
 
     private void MoveOnClick(){
